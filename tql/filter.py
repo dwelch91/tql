f3c4f49518b0074cd2c4bb367582b6d2b24b4104
dwelch91 <dwelch91@gmail.com>
2018-11-19 09:31:10 -0800

    Added trim filters. Re-formatting.

--- tql/filter.py
+++ tql/filter.py
@@ -10,16 +10,15 @@ from tql.utils import to_num, to_int, to_float, humanize, dehumanize, ordinal
 from tql.replace import apply_char_replacements
 
 FILTERS = {
-    # Data conversion
+    # Data type conversion
     'num': (to_num, 0, "<col>|num", "str", "num", "Convert to integer or float."),
     'number': (to_num, 0, "<col>|number", "str", "num", "Convert to integer or float."),
     'int': (to_int, 0, "<col>|int", "str", "int", "Convert to integer."),
     'float': (to_float, 0, "<col>|float", "str", "float", "Convert to float."),
-    'str': (lambda s: str(s), 0, "<col>|str", "any", "str", "Convert to string."),
+    'str': (str, 0, "<col>|str", "any", "str", "Convert to string."),
 
     # Human
-    'humanize': (lambda s, u: humanize(to_num(s), u, show_value=False), 1,
-                 "<col>|humanize:<unit>", "num", "str", "Format number to human string."),
+    'humanize': (lambda s, u: humanize(to_num(s), u, show_value=False), 1, "<col>|humanize:<unit>", "num", "str", "Format number to human string."),
     'dehumanize': (dehumanize, 0, "<col>|dehumanize", "str", "num", "Convert from human string to number."),
     'ordinal': (lambda s: ordinal(to_int(s)), 0, "<col>|ordinal", "num", "str", "Convert number to ordinal string."),
 
@@ -27,27 +26,22 @@ FILTERS = {
     'lower': (lambda s: s.lower(), 0, "<col>|lower", "str", "str", "Convert string to lowercase."),
     'upper': (lambda s: s.upper(), 0, "<col>|upper", "str", "str", "Convert string to uppercase."),
     'capitalize': (lambda s: s.capitalize(), 0, "<col>|capitalize", "str", "str", "Capitalize string."),
-    'ljust': (lambda s, w: s.ljust(to_int(w)), 1, "<col>|ljust:<width>", "str", "str",
-              "Left justify string in <width> spaces."),
-    'rjust': (lambda s, w: s.rjust(to_int(w)), 1, "<col>|rjust:<width>", "str", "str",
-              "Right justify string in <width> spaces."),
-    'center': (lambda s, w: s.center(to_int(w), ' '), 1, "<col>|center:<width>", "str", "str",
-               "Center string in <width> spaces."),
+    'ljust': (lambda s, w: s.ljust(to_int(w)), 1, "<col>|ljust:<width>", "str", "str", "Left justify string in <width> spaces."),
+    'rjust': (lambda s, w: s.rjust(to_int(w)), 1, "<col>|rjust:<width>", "str", "str", "Right justify string in <width> spaces."),
+    'center': (lambda s, w: s.center(to_int(w), ' '), 1, "<col>|center:<width>", "str", "str", "Center string in <width> spaces."),
     'swapcase': (lambda s: s.swapcase(s), 0, "<col>|swapcase", "str", "str", "Swap string case."),
-    'replace': (lambda s, x, y: s.replace(x, y), 2, "<col>|replace:<from>,<to>", "str", "str",
-                "Replace sub-string <from> with <to>."),
+    'replace': (lambda s, x, y: s.replace(x, y), 2, "<col>|replace:<from>,<to>", "str", "str", "Replace sub-string <from> with <to>."),
     'title': (lambda s: s.title(), 0, "<col>|title", "str", "str", "Convert string to title case."),
-    'zfill': (lambda s, w: s.zfill(to_int(w)), 1, "<col>|zfill:<width>", "str", "str",
-              "Zero fill string to <width> size."),
+    'zfill': (lambda s, w: s.zfill(to_int(w)), 1, "<col>|zfill:<width>", "str", "str", "Zero fill string to <width> size."),
     'length': (lambda s: len(s), 0, "<col>|length", "str", "str", "Return the length of the string."),
     'reverse': (lambda s: ''.join(reversed(s)), 0, "<col>|reverse", "str", "str", "Reverse the characters in the string."),
     'prefix': (lambda s, p: ''.join([p, s]), 1, "<col>|prefix:<prefix>", "str", "str", "Prefix the string with <prefix>."),
     'suffix': (lambda s, x: ''.join([s, x]), 1, "<col>|suffix:<suffix>", "str", "str", "Suffix the string with <suffix>."),
     'substr': (lambda s, x, y: s[x:y], 2, "<col>|substr:<start>,<end>", "str", "str", "Return a sub-string."),
-    'lstrip': (lambda s, c: s.lstrip(c), 1, "<col>|lstrip:<chars>", "str", "str",
-               "Strip <chars> from the left end of the string."),
-    'rstrip': (lambda s, c: s.rstrip(c), 1, "<col>|rstrip:<chars>", "str", "str",
-               "Strip <chars> from the right end of the string."),
+    'lstrip': (lambda s, c: s.lstrip(c), 1, "<col>|lstrip:<chars>", "str", "str", "Strip <chars> from the left end of the string."),
+    'ltrim': (lambda s: s.lstrip(), 1, "<col>|ltrim", "str", "str", "Strip whitespace characters from the left end of the string."),
+    'rstrip': (lambda s, c: s.rstrip(c), 1, "<col>|rstrip:<chars>", "str", "str", "Strip <chars> from the right end of the string."),
+    'rtrim': (lambda s: s.rstrip(), 1, "<col>|rtrim", "str", "str", "Strip whitespace characters from the right end of the string."),
     'squotes': (lambda s: f"'{s}", 0, '<col>|squote', "str", "str", "Wrap a string in single quotes."),
     'dquotes': (lambda s: f'"{s}"', 0, '<col>|dquote', "str", "str", "Wrap a string in double quotes."),
     'backticks': (lambda s: f'`{s}`', 0, '<col>|backticks', "str", "str", "Wrap a string in backticks."),
@@ -55,8 +49,7 @@ FILTERS = {
     # TODO: regex
 
     # Data formatting
-    'format': (lambda x, fmt: format(to_num(x), fmt), 1, "<col>|format:<format>", "str", "str",
-               "Format data using Python's `format(<format>)` function."),
+    'format': (lambda x, fmt: format(to_num(x), fmt), 1, "<col>|format:<format>", "str", "str", "Format data using Python's `format(<format>)` function."),
     # TODO: locale:
     # 'format_currency': (),
     # 'format_number': (),
@@ -68,24 +61,18 @@ FILTERS = {
     'mult': (lambda s, o: to_num(s) * to_num(o), 1, "<col>|mult:<value>", "num", "num", "Multiply number by <value>."),
     'div': (lambda s, o: to_num(s) / to_num(o), 1, "<col>|div:<value>", "num", "num", "Divide number by <value>."),
     'abs': (lambda x: abs(to_num(x)), 0, "<col>|abs", "num", "num", "Take the absolute value of a number."),
-    'round': (lambda x, d: round(to_num(x), to_int(d)), 1, "<col>|round:<digits>", "num", "num",
-              "Round number to <digits> digits."),
+    'round': (lambda x, d: round(to_num(x), to_int(d)), 1, "<col>|round:<digits>", "num", "num", "Round number to <digits> digits."),
     'ceil': (lambda x: ceil(to_num(x)), 0, "<col>|ceil", "num", "num", "Return the ceiling value of a number."),
     'floor': (lambda x: floor(to_num(x)), 0, "<col>|floor", "num", "num", "Return the floor value of a number."),
     'trunc': (lambda x: trunc(to_num(x)), 0, "<col>|trunc", "num", "num", "Return the number truncated."),
 
     # Datetime filters (intermediate values are pendulum datetimes)
     'datetime': (lambda s: pendulum.parse(s), 0, "<col>|datetime", "str", "datetime", "Parse a datetime string."),
-    'datetime_tz': (lambda s, tz: pendulum.parse(s, tz=tz), 1, "<col>|datetime_tz:<tz>", "str", "datetime",
-                    "Prase a datetime string with the specified <tz> timezone."),
-    'tz': (lambda dt, tz: pendulum.timezone(tz).convert(dt), 1, "<col>|tz:<tz>", "datetime", "datetime",
-           "Convert a datetime to a new <tz> timezone."),
-    'iso8601': (lambda dt: dt.to_iso8601_string(), 0, "<col>|iso8601", "datetime", "str",
-                "Convert a datetime to an ISO8601 string representation."),
-    'utc': (lambda dt: pendulum.timezone('UTC').convert(dt), 0, "<col>|utc", "datetime", "datetime",
-            "Convert a datetime to UTC."),
-    'strftime': (lambda dt, fmt: dt.strftime(fmt), 1, "<col>|strftime:<format>", "datetime", "str",
-                 "Format a datetime using `strftime(<format>)`."),
+    'datetime_tz': (lambda s, tz: pendulum.parse(s, tz=tz), 1, "<col>|datetime_tz:<tz>", "str", "datetime", "Parse a datetime string with the specified <tz> timezone."),
+    'tz': (lambda dt, tz: pendulum.timezone(tz).convert(dt), 1, "<col>|tz:<tz>", "datetime", "datetime", "Convert a datetime to a new <tz> timezone."),
+    'iso8601': (lambda dt: dt.to_iso8601_string(), 0, "<col>|iso8601", "datetime", "str", "Convert a datetime to an ISO8601 string representation."),
+    'utc': (lambda dt: pendulum.timezone('UTC').convert(dt), 0, "<col>|utc", "datetime", "datetime", "Convert a datetime to UTC."),
+    'strftime': (lambda dt, fmt: dt.strftime(fmt), 1, "<col>|strftime:<format>", "datetime", "str", "Format a datetime using `strftime(<format>)`."),
 }
 
 
